ed839f4829a8f090b162c159a067a60c43eb0b08
Vlad <vladkopachovets@ukr.net>
2017-12-08 14:18:57 +0100

    test-commit

--- app/src/main/java/com/example/smeet/scrum_support/activity/LoginActivity.java
+++ app/src/main/java/com/example/smeet/scrum_support/activity/LoginActivity.java
@@ -38,8 +38,8 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initializeForm();
-        initClickAction();
+//        initializeForm();
+//        initClickAction();
 
     }
 
@@ -53,81 +53,81 @@ public class LoginActivity extends AppCompatActivity {
         error = (TextView) findViewById(R.id.error);
     }
 
-    private void initClickAction() {
-        btnCreateSession.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    if(checkData()) {
-                        createSession();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Fields must contains more than 1 char", Toast.LENGTH_SHORT).show();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
+//    private void initClickAction() {
+//        btnCreateSession.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                try {
+//                    if(checkData()) {
+//                        createSession();
+//                    } else {
+//                        Toast.makeText(getApplicationContext(), "Fields must contains more than 1 char", Toast.LENGTH_SHORT).show();
+//                    }
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        });
+//
+//        btnConnectSession.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                try {
+//                    if(checkData()) {
+//                        connectSession();
+//                    } else {
+//                        Toast.makeText(getApplicationContext(), "Fields must contains more than 1 char", Toast.LENGTH_SHORT).show();
+//                    }
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+//
+//    private void connectSession() throws SQLException {
+//
+//        Session session = sessionDao.connectSession(sessionName.getText().toString(), password.getText().toString());
+//        if(session != null) {
+//            if(isScrumMaster.isChecked()) {
+//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                intent.putExtra("sessionId", session.getId());
+//                startActivity(intent);
+//            } else {
+//                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
+//                intent.putExtra("sessionId", session.getId());
+//                startActivity(intent);
+//            }
+//
+//        } else {
+//            Toast.makeText(getApplicationContext(), "Session with this name not exist", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
-            }
-        });
-
-        btnConnectSession.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                try {
-                    if(checkData()) {
-                        connectSession();
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Fields must contains more than 1 char", Toast.LENGTH_SHORT).show();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void connectSession() throws SQLException {
-
-        Session session = sessionDao.connectSession(sessionName.getText().toString(), password.getText().toString());
-        if(session != null) {
-            if(isScrumMaster.isChecked()) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("sessionId", session.getId());
-                startActivity(intent);
-            } else {
-                Intent intent = new Intent(getApplicationContext(), UserActivity.class);
-                intent.putExtra("sessionId", session.getId());
-                startActivity(intent);
-            }
-
-        } else {
-            Toast.makeText(getApplicationContext(), "Session with this name not exist", Toast.LENGTH_SHORT).show();
-        }
-    }
 
 
-
-    private void createSession() throws SQLException {
-
-        Session session = sessionDao.getByName(sessionName.getText().toString());
-        if(session == null) {
-                if(sessionDao.createSession(sessionName.getText().toString(), password.getText().toString())) {
-                    Toast.makeText(getApplicationContext(), "Session created", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Something went wrong ", Toast.LENGTH_SHORT).show();
-                }
-        } else {
-            Toast.makeText(getApplicationContext(), "Session with this name already exist ", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private boolean checkData() {
-        if(sessionName.getText().toString().length() > 1 && password.getText().toString().length() > 1) {
-            return true;
-        }
-        return false;
-    }
+//    private void createSession() throws SQLException {
+//
+//        Session session = sessionDao.getByName(sessionName.getText().toString());
+//        if(session == null) {
+//                if(sessionDao.createSession(sessionName.getText().toString(), password.getText().toString())) {
+//                    Toast.makeText(getApplicationContext(), "Session created", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Something went wrong ", Toast.LENGTH_SHORT).show();
+//                }
+//        } else {
+//            Toast.makeText(getApplicationContext(), "Session with this name already exist ", Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    private boolean checkData() {
+//        if(sessionName.getText().toString().length() > 1 && password.getText().toString().length() > 1) {
+//            return true;
+//        }
+//        return false;
+//    }
 
 }
 
