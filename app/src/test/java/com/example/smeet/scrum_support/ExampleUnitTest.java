4ca3c566a4667ad644ebbe06cc71668166791ddf
AndriiChemer <ichemer@icloud.com>
2017-12-08 16:33:52 +0200

    add - StoryDaoImplTest a few methods

--- app/src/test/java/com/example/smeet/scrum_support/ExampleUnitTest.java
+++ app/src/test/java/com/example/smeet/scrum_support/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.example.smeet.scrum_support;
 
+import com.example.smeet.scrum_support.dao.impl.StoryDaoImpl;
+import com.example.smeet.scrum_support.model.Story;
+
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +15,23 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    StoryDaoImpl storyDao = new StoryDaoImpl();
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void getStory() throws Exception {
+        List<Story> stories = storyDao.getAll();
+        assertNotNull("Stories not found", stories);
+        System.out.println("Category list size: " + stories.size());
+    }
+
+    @Test
+    public void getStoryById() throws Exception {
+        Story stories = storyDao.getById(1);
+        assertNotNull("Story not found", stories);
+        System.out.println("Story id: " + stories.getId());
     }
 }
