e6e30a6dc86cb4ccac9dd327e5f6028d62159203
AndriiSmeet <Andryuha955@gmail.com>
2017-12-09 16:27:58 +0100

    add async task for session

---
+++ app/src/main/java/com/example/smeet/scrum_support/async/AsyncConnectSession.java
@@ -0,0 +1,71 @@
+package com.example.smeet.scrum_support.async;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.example.smeet.scrum_support.R;
+import com.example.smeet.scrum_support.activity.ScrumMasterActivity;
+import com.example.smeet.scrum_support.dao.SessionDao;
+import com.example.smeet.scrum_support.dao.impl.SessionDaoImpl;
+import com.example.smeet.scrum_support.model.Session;
+
+/**
+ * Created by Smeet on 09.12.2017.
+ */
+
+public class AsyncConnectSession extends AsyncTask<Integer, Integer, Void> {
+
+    private Context context;
+    private SessionDao sessionDao;
+    private String name;
+    private String password;
+    private Session session;
+    private ProgressDialog progressDialog;
+
+    public AsyncConnectSession(Context context, String name, String password) {
+        this.context = context;
+        this.sessionDao = new SessionDaoImpl();
+        this.name = name;
+        this.password = password;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+
+        progressDialog = new ProgressDialog(context);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_bar);
+    }
+
+    @Override
+    protected Void doInBackground(Integer... integers) {
+        connect();
+        return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(Void s) {
+
+        if(session == null) {
+            Toast.makeText(context, "Connection with this data do not exist", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(context, ScrumMasterActivity.class);
+            intent.putExtra("sessionName", session.getSessionName());
+            intent.putExtra("sessionId", session.getId());
+            context.startActivity(intent);
+        }
+
+        progressDialog.dismiss();
+        super.onPostExecute(s);
+    }
+
+    private void connect() {
+        session = sessionDao.connect(name, password);
+    }
+
+}
---
+++ app/src/main/java/com/example/smeet/scrum_support/async/AsyncCreateSession.java
@@ -0,0 +1,62 @@
+package com.example.smeet.scrum_support.async;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.example.smeet.scrum_support.R;
+import com.example.smeet.scrum_support.dao.SessionDao;
+import com.example.smeet.scrum_support.dao.impl.SessionDaoImpl;
+import com.example.smeet.scrum_support.model.Session;
+
+/**
+ * Created by Smeet on 09.12.2017.
+ */
+
+public class AsyncCreateSession extends AsyncTask<Integer, Integer, Void> {
+
+    private Context context;
+    private SessionDao sessionDao;
+    private Session session;
+    private ProgressDialog progressDialog;
+    private Integer sessionId;
+
+    public AsyncCreateSession(Context context, Session session) {
+        this.context = context;
+        this.sessionDao = new SessionDaoImpl();
+        this.session = session;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        progressDialog = new ProgressDialog(context);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_bar);
+    }
+
+    @Override
+    protected Void doInBackground(Integer... integers) {
+        create();
+        return null;
+
+    }
+
+    @Override
+    protected void onPostExecute(Void s) {
+
+        if(sessionId == null) {
+            Toast.makeText(context, "Session with this name already exist", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Session successfully created", Toast.LENGTH_SHORT).show();
+        }
+        progressDialog.dismiss();
+        super.onPostExecute(s);
+    }
+
+    private void create() {
+        sessionId = sessionDao.create(session);
+    }
+
+}
