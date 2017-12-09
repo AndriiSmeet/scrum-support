08c4e8530d56af8e0256be9fa12a9fb71461750e
AndriiChemer <ichemer@icloud.com>
2017-12-09 20:45:55 +0200

    update - StoryService StoryServiceImpl and a few methods

--- app/src/main/java/com/example/smeet/scrum_support/service/StoryService.java
+++ app/src/main/java/com/example/smeet/scrum_support/service/StoryService.java
@@ -1,8 +1,17 @@
 package com.example.smeet.scrum_support.service;
 
+import com.example.smeet.scrum_support.model.Story;
+
+import java.util.List;
+
 /**
  * Created by Smeet on 08.12.2017.
  */
 
 public interface StoryService {
+    Story getById(Integer id);
+    Integer create(Story story);
+    List<Story> getAll();
+    List<Story> getAllStoryByIdSession(Integer idSession);
+    List<Story> getAllStoryByIsActive(boolean arg);
 }
--- app/src/main/java/com/example/smeet/scrum_support/service/impl/StoryServiceImpl.java
+++ app/src/main/java/com/example/smeet/scrum_support/service/impl/StoryServiceImpl.java
@@ -1,10 +1,58 @@
 package com.example.smeet.scrum_support.service.impl;
 
+import android.content.Context;
+
+import com.example.smeet.scrum_support.async.story.AsyncCreateStory;
+import com.example.smeet.scrum_support.async.story.AsyncGetAllStoryByIdSession;
+import com.example.smeet.scrum_support.async.story.AsyncGetAllStoryByIsActive;
+import com.example.smeet.scrum_support.async.story.AsyncGetByIdStory;
+import com.example.smeet.scrum_support.model.Story;
 import com.example.smeet.scrum_support.service.StoryService;
+
+import java.util.List;
 
 /**
  * Created by Smeet on 08.12.2017.
  */
 
 public class StoryServiceImpl implements StoryService {
+
+    Context context;
+
+    public StoryServiceImpl(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public Story getById(Integer id) {
+        AsyncGetByIdStory asyncTask = new AsyncGetByIdStory(id, context);
+        asyncTask.execute();
+        return null;
+    }
+
+    @Override
+    public Integer create(Story story) {
+        AsyncCreateStory asyncTask = new AsyncCreateStory(story, context);
+        asyncTask.execute();
+        return null;
+    }
+
+    @Override
+    public List<Story> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Story> getAllStoryByIdSession(Integer idSession) {
+        AsyncGetAllStoryByIdSession asyncTask = new AsyncGetAllStoryByIdSession(context, idSession);
+        asyncTask.execute();
+        return null;
+    }
+
+    @Override
+    public List<Story> getAllStoryByIsActive(boolean arg) {
+        AsyncGetAllStoryByIsActive asyncTask = new AsyncGetAllStoryByIsActive(context, arg);
+        asyncTask.execute();
+        return null;
+    }
 }
